9b0d03ffc72346bbd254e8c745bff74db551d5d9
PaterOHerlihy <50612434+PaterOHerlihy@users.noreply.github.com>
2022-02-02 23:02:16 +0100

    Changed for test purposes

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 "use strict"
 
+
+
 $(document).ready(function() {
     $.ajax({
         url: "http://localhost:8080/activities",
+        dataType: "json"
     }).then(function(data) {
-       console.log(data);
+        console.log(typeof data);
+       appendRecords(data);
     });
+
+    function appendRecords(data){
+
+        for(let i = 0; i < data.length; i++) {
+            let obj = data[i];
+        
+            console.log(obj);
+        }
+            
+            let li = $.parseHTML("<li> " + data[0] + " </li>");
+            $("#records").append(li);
+    
+        
+    }
 });
+
+
